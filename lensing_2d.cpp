ec92fe6ac40578a83e5258cf2001034fed264c69
omedcodes <omedsafi21@gmail.com>
2026-04-07 16:15:56 +0200

    Feat: complete geodesicRHS calculatiom and integrate main loop

--- lensing_2d.cpp
+++ lensing_2d.cpp
@@ -90,9 +90,9 @@ struct BlackHole {
 };
 BlackHole SagA(vec3(0.0f, 0.0f, 0.0f), 8.54e36); // Sagittarius A black hole
 struct Ray{
-    double x;   double y;
-    double r;   double phi;
-    double dr;  double dphi;
+    double x;  double y;
+    double r;  double phi;
+    double dr; double dphi;
     vector<vec2> trail;
     double E, L;
 
@@ -141,7 +141,6 @@ struct Ray{
         glDisable(GL_BLEND);
     }
     void step(double dλ, double rs) {
-        // 1) integrate (r,φ,dr,dφ)
         if(r <= rs) return;
         rk4Step(*this, dλ, rs);
         x = r * cos(phi);
@@ -150,3 +149,67 @@ struct Ray{
     }
 };
 vector<Ray> rays;
+
+
+void geodesicRHS(const Ray& ray, double rhs[4], double rs) {
+    double r=ray.r;
+    double dr=ray.dr;
+    double dphi=ray.dphi;
+    double E=ray.E;
+    double f = 1.0 - rs/r;
+
+    // dr/dλ = dr
+    rhs[0] = dr;
+    rhs[1] = dphi;
+
+    double dt_dλ = E / f;
+    rhs[2] = 
+        - (rs/(2*r*r)) * f * (dt_dλ*dt_dλ)
+        + (rs/(2*r*r*f)) * (dr*dr)
+        + (r - rs) * (dphi*dphi);
+
+    // d²φ/dλ² = -2*(dr * dphi) / r
+    rhs[3] = -2.0 * dr * dphi / r;
+}
+void addState(const double a[4], const double b[4], double factor, double out[4]) {
+    for (int i = 0; i < 4; i++)
+        out[i] = a[i] + b[i] * factor;
+}
+void rk4Step(Ray& ray, double dλ, double rs) {
+    double y0[4] = { ray.r, ray.phi, ray.dr, ray.dphi };
+    double k1[4], k2[4], k3[4], k4[4], temp[4];
+
+    geodesicRHS(ray, k1, rs);
+    addState(y0, k1, dλ/2.0, temp);
+    Ray r2 = ray; r2.r=temp[0]; r2.phi=temp[1]; r2.dr=temp[2]; r2.dphi=temp[3];
+    geodesicRHS(r2, k2, rs);
+
+    addState(y0, k2, dλ/2.0, temp);
+    Ray r3 = ray; r3.r=temp[0]; r3.phi=temp[1]; r3.dr=temp[2]; r3.dphi=temp[3];
+    geodesicRHS(r3, k3, rs);
+
+    addState(y0, k3, dλ, temp);
+    Ray r4 = ray; r4.r=temp[0]; r4.phi=temp[1]; r4.dr=temp[2]; r4.dphi=temp[3];
+    geodesicRHS(r4, k4, rs);
+
+    ray.r += (dλ/6.0)*(k1[0] + 2*k2[0] + 2*k3[0] + k4[0]);
+    ray.phi += (dλ/6.0)*(k1[1] + 2*k2[1] + 2*k3[1] + k4[1]);
+    ray.dr += (dλ/6.0)*(k1[2] + 2*k2[2] + 2*k3[2] + k4[2]);
+    ray.dphi+=(dλ/6.0)*(k1[3] + 2*k2[3] + 2*k3[3] + k4[3]);
+}
+
+
+int main () {
+    //rays.push_back(Ray(vec2(-1e11, 3.27606302719999999e10), vec2(c, 0.0f)));
+    while(!glfwWindowShouldClose(engine.window)) {
+        engine.run();
+        SagA.draw();
+        for (auto& ray : rays) {
+            ray.step(1.0f, SagA.r_s);
+            ray.draw(rays);
+        }
+        glfwSwapBuffers(engine.window);
+        glfwPollEvents();
+    }
+    return 0;
+}
